fca32a1530b7acbbeb45446ade4a897b7d2918c0
Asqar <Asqar95>
2023-06-18 11:08:06 +0500

    edit auth

--- internal/transport/rest/auth.go
+++ internal/transport/rest/auth.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"errors"
 	"github.com/Asqar95/crud-app/internal/domain"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -22,24 +23,20 @@ func (h *Handler) singUp(c *gin.Context) {
 
 }
 
-type singInInput struct {
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
-
 func (h *Handler) singIn(c *gin.Context) {
-	var input singInInput
+	var inp domain.SignInInput
 
-	if err := c.BindJSON(&input); err != nil {
+	if err := c.BindJSON(&inp); err != nil {
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	token, err := h.userService.ParseToken(input.Username, input.Password)
+	token, err := h.userService.SignIn(c, inp)
 	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
-		return
-	}
+		if errors.Is(err, domain.ErrUserNotFound) {
+			handleNotFoundError(w, err)
+			return
+		}
 
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"token": token,
